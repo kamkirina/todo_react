5a475477c5361cb83f3f06a494ac417072034ce3
Irina <kamkina2312@gmail.com>
2023-08-10 16:39:52 +0300

    add complited and edit functional

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -2,18 +2,55 @@ import { Component } from "react";
 import "./Task.css";
 
 export default class Task extends Component {
+  state = {
+    completed: false,
+    editing: false,
+  };
+
+  oncheckboxClick = () => {
+    this.setState((state) => {
+      return {
+        completed: !state.completed,
+      };
+    });
+  };
+
+  onEditClick = () => {
+    this.setState({
+      editing: true,
+    });
+  };
+
   render() {
     const { description, time } = this.props;
+    const { completed, editing } = this.state;
+
+    let liClassName = "";
+    if (completed) {
+      liClassName = "completed";
+    }
+
+    if (editing) {
+      liClassName = "editing";
+      return <input type="text" class="edit" value="Editing task" />;
+    }
+
     return (
-      <div class="view">
-        <input class="toggle" type="checkbox" />
-        <label>
-          <span class="description">{description}</span>
-          <span class="created">{time}</span>
-        </label>
-        <button class="icon icon-edit"></button>
-        <button class="icon icon-destroy"></button>
-      </div>
+      <li className={liClassName}>
+        <div class="view">
+          <input
+            class="toggle"
+            type="checkbox"
+            onClick={this.oncheckboxClick}
+          />
+          <label>
+            <span class="description">{description}</span>
+            <span class="created">{time}</span>
+          </label>
+          <button class="icon icon-edit" onClick={this.onEditClick}></button>
+          <button class="icon icon-destroy"></button>
+        </div>
+      </li>
     );
   }
 }
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,29 +2,13 @@ import Task from "../Task";
 import "./TaskList.css";
 
 const TaskList = ({ todos }) => {
-  const elements = todos.map((item) => {
-    const { completed, editing, ...itemProps } = item;
-    if (completed) {
-      return (
-        <li className="completed">
-          <Task {...itemProps} />
-        </li>
-      );
-    }
-    if (editing) {
-      return (
-        <li className="editing">
-          <Task {...itemProps} />
-          <input type="text" class="edit" value="Editing task" />
-        </li>
-      );
-    }
-    return (
-      <li>
-        <Task {...itemProps} />
-      </li>
-    );
+  const elements = [];
+  todos.forEach((el) => {
+    console.log(el);
+    elements.push(<Task {...el} />);
+    return elements;
   });
+  console.log(elements);
   return <ul class="todo-list">{elements}</ul>;
 };
 
