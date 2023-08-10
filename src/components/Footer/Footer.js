780c94374890d9b8f16e02affa4be1ce01b7d894
Irina <kamkina2312@gmail.com>
2023-08-10 13:54:14 +0300

    add class Footer and Task

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,24 @@
+import { Component } from "react";
 import "./Footer.css";
 
-const Footer = () => {
-  return (
-    <footer class="footer">
-      <span class="todo-count">1 items left</span>
-      <ul class="filters">
-        <li>
-          <button class="selected">All</button>
-        </li>
-        <li>
-          <button>Active</button>
-        </li>
-        <li>
-          <button>Completed</button>
-        </li>
-      </ul>
-      <button class="clear-completed">Clear completed</button>
-    </footer>
-  );
-};
-
-export default Footer;
+export default class Footer extends Component {
+  render() {
+    return (
+      <footer class="footer">
+        <span class="todo-count">1 items left</span>
+        <ul class="filters">
+          <li>
+            <button class="selected">All</button>
+          </li>
+          <li>
+            <button>Active</button>
+          </li>
+          <li>
+            <button>Completed</button>
+          </li>
+        </ul>
+        <button class="clear-completed">Clear completed</button>
+      </footer>
+    );
+  }
+}
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,17 +1,19 @@
+import { Component } from "react";
 import "./Task.css";
 
-const Task = ({ description, time }) => {
-  return (
-    <div class="view">
-      <input class="toggle" type="checkbox" />
-      <label>
-        <span class="description">{description}</span>
-        <span class="created">{time}</span>
-      </label>
-      <button class="icon icon-edit"></button>
-      <button class="icon icon-destroy"></button>
-    </div>
-  );
-};
-
-export default Task;
+export default class Task extends Component {
+  render() {
+    const { description, time } = this.props;
+    return (
+      <div class="view">
+        <input class="toggle" type="checkbox" />
+        <label>
+          <span class="description">{description}</span>
+          <span class="created">{time}</span>
+        </label>
+        <button class="icon icon-edit"></button>
+        <button class="icon icon-destroy"></button>
+      </div>
+    );
+  }
+}
